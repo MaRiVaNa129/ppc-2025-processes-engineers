5151b7ef565a4f53147ca086598fa6d7adb76141
MaRiVaNa129 <132110214+MaRiVaNa129@users.noreply.github.com>
2025-11-18 14:10:51 +0300

    test 5

--- tasks/shkenev_i_diff_betw_neighb_elem_vec/seq/src/ops_seq.cpp
+++ tasks/shkenev_i_diff_betw_neighb_elem_vec/seq/src/ops_seq.cpp
@@ -25,13 +25,13 @@ bool ShkenevIDiffBetwNeighbElemVecSEQ::PreProcessingImpl() {
 
 bool ShkenevIDiffBetwNeighbElemVecSEQ::RunImpl() {
   std::vector<int> &vec = GetInput();
-  if (vec.size() < 2) {
+  int n = vec.size();
+  if (n < 2) {
     GetOutput() = 0;
     return true;
   }
 
   int max_diff = 0;
-  int n = vec.size();
   for (int i = 0; i < n - 1; i++) {
     int diff = std::abs(vec[i + 1] - vec[i]);
     if (diff > max_diff) {
