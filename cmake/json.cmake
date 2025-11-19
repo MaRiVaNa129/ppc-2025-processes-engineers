3adaa42d125a69f082f5561fbbcacb9ae262c55e
MaRiVaNa129 <132110214+MaRiVaNa129@users.noreply.github.com>
2025-11-19 19:01:21 +0300

    Fix json3

--- cmake/json.cmake
+++ cmake/json.cmake
@@ -1,11 +1,8 @@
 include(ExternalProject)
 
-get_filename_component(JSON_SOURCE_DIR
-                       "${CMAKE_CURRENT_LIST_DIR}/../3rdparty/json" ABSOLUTE)
-
 ExternalProject_Add(
   ppc_json
-  SOURCE_DIR "${JSON_SOURCE_DIR}"
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/json"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/ppc_json"
   BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
   INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install"
