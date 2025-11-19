2b300c35dc8d7cd72f56d964d5f511349ba9bb4e
MaRiVaNa129 <132110214+MaRiVaNa129@users.noreply.github.com>
2025-11-19 20:17:21 +0300

    Fix json and clang3

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,39 +1,32 @@
 include(ExternalProject)
 
 ExternalProject_Add(
-  ppc_googletest
-  SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/googletest"
-  PREFIX "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest"
-  BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/build"
-  INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/install"
+  ppc_json
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/json"
+  PREFIX "${CMAKE_CURRENT_BINARY_DIR}/ppc_json"
+  BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
+  INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install"
   CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
              -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
              -DCMAKE_C_COMPILER_LAUNCHER=${CMAKE_C_COMPILER_LAUNCHER}
              -DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}
              -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
-             -DCMAKE_CXX_STANDARD_REQUIRED=${CMAKE_CXX_STANDARD_REQUIRED}
+             -DCMAKE_CXX_STANDARD_REQUIRED=ON
              -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-             -DCMAKE_C_FLAGS=-w
-             -DCMAKE_CXX_FLAGS=-w
-             -DBUILD_GMOCK=OFF
-             $<$<BOOL:MSVC>:-Dgtest_force_shared_crt=ON>
+             -DJSON_BuildTests=OFF
   BUILD_COMMAND
-    "${CMAKE_COMMAND}" --build
-    "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/build" --config $<CONFIG>
-    --parallel
+    "${CMAKE_COMMAND}" --build "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
+    --config $<CONFIG> --parallel
   INSTALL_COMMAND
-    "${CMAKE_COMMAND}" --install
-    "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/build" --config $<CONFIG>
-    --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_googletest/install")
+    "${CMAKE_COMMAND}" --install "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/build"
+    --config $<CONFIG> --prefix "${CMAKE_CURRENT_BINARY_DIR}/ppc_json/install")
 
-function(ppc_link_gtest exec_func_lib)
+function(ppc_link_json exec_func_lib)
   # Add external project include directories
-  target_include_directories(
-    ${exec_func_lib}
-    PUBLIC ${CMAKE_SOURCE_DIR}/3rdparty/googletest/googletest/include)
+  target_include_directories(${exec_func_lib}
+                             PUBLIC ${CMAKE_SOURCE_DIR}/3rdparty/json/include)
 
-  add_dependencies(${exec_func_lib} ppc_googletest)
-  target_link_directories(${exec_func_lib} PUBLIC
-                          "${CMAKE_BINARY_DIR}/ppc_googletest/install/lib")
-  target_link_libraries(${exec_func_lib} PUBLIC gtest gtest_main)
+  add_dependencies(${exec_func_lib} ppc_json)
+  target_include_directories(
+    ${exec_func_lib} INTERFACE "${CMAKE_BINARY_DIR}/ppc_json/install/include")
 endfunction()
