604380bd1ba4312470bd65feb9fe055acc50ecd7
MaRiVaNa129 <132110214+MaRiVaNa129@users.noreply.github.com>
2025-11-20 15:18:46 +0300

    Fix files

--- scripts/generate_perf_results.sh
+++ scripts/generate_perf_results.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Создаём директорию для результатов
 mkdir -p build/perf_stat_dir
-
-# Запускаем тесты и сохраняем лог
 scripts/run_tests.py --running-type="performance" | tee build/perf_stat_dir/perf_log.txt
-
-# Создаём таблицу результатов
 python3 scripts/create_perf_table.py --input build/perf_stat_dir/perf_log.txt --output build/perf_stat_dir
