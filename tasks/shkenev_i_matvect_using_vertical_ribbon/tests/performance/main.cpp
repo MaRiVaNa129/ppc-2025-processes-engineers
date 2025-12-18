ac9d1f678d63172b92a85cb151e091bc2ac9882e
MaRiVaNa129 <132110214+MaRiVaNa129@users.noreply.github.com>
2025-12-18 06:47:10 +0300

    fix mac

--- tasks/shkenev_i_matvect_using_vertical_ribbon/tests/performance/main.cpp
+++ tasks/shkenev_i_matvect_using_vertical_ribbon/tests/performance/main.cpp
@@ -13,7 +13,7 @@ namespace shkenev_i_matvect_using_vertical_ribbon {
 
 class ShkenevImatvectUsingVerticalRibbonPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
  public:
-  static constexpr size_t kSize = 11000;
+  static constexpr size_t kSize = 9000;
 
  protected:
   void SetUp() override {
